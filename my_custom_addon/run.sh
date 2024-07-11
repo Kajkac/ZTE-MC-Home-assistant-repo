ac6300d2f9f806c30dd185e981ca0279d282b5eb
Your Name <you@example.com>
2024-07-11 21:40:41 +0200

    TEST 3

--- my_custom_addon/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-# Copy custom components to the Home Assistant config directory
-mkdir -p /config/custom_components/zte_router
-cp -r /custom_components/zte_router/* /config/custom_components/zte_router/
-
-echo "Custom integration has been installed."
